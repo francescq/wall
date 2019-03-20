c5dd2711e3b6fee2041ffc2c168ded9d9bb7ea3a
Francesc <francescq@gmail.com>
2019-03-20 18:17:33 +0100

    add assets

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Bag from '../../images/Bag.png';
 
 const App = () => {
-    return <div>App</div>;
+    return (
+        <div>
+            App
+            <img src={Bag} />
+        </div>
+    );
 };
 
 export default App;
