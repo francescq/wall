5e2637727fd4d6ef567b6dc7182ba20bc52b4d3f
Francesc <francescq@gmail.com>
2019-03-22 13:05:03 +0100

    itemsapi refactor

--- src/api/ItemsApi.js
+++ src/api/ItemsApi.js
@@ -7,36 +7,51 @@ export default class ItemsApi {
         //console.log('fetching items', query, pagination);
         const { page, size } = pagination;
 
-        const answer = this.db.items.filter(e => {
-            if (!query) {
-                return false;
-            }
+        const queryItem = () => {
+            return this.db.items.filter(e => {
+                if (!query) {
+                    return false;
+                }
 
-            const regex = new RegExp(query, 'i');
+                const regex = new RegExp(query, 'i');
 
-            return (
-                e.title.match(regex) ||
-                e.description.match(regex) ||
-                e.price.match(regex) ||
-                e.email.match(regex)
-            );
-        });
+                return (
+                    e.title.match(regex) ||
+                    e.description.match(regex) ||
+                    e.price.match(regex) ||
+                    e.email.match(regex)
+                );
+            });
+        };
 
-        const willFetch = new Promise(resolve => {
-            const result = {};
-            result.page = {};
+        const getPageMeta = (answer, page, size) => {
+            const pageMeta = {};
 
-            result.page.size = size;
+            pageMeta.size = size;
 
             const totalPages = Math.ceil(answer.length / size);
 
-            result.page.totalPages = totalPages;
-            result.page.page = page < totalPages ? page : totalPages - 1;
+            pageMeta.totalPages = totalPages;
+            pageMeta.page = page < totalPages ? page : totalPages - 1;
+
+            return pageMeta;
+        };
+
+        const applyOrder = answer => {
+            return answer;
+        };
+
+        const willFetch = new Promise(resolve => {
+            const result = {};
+
+            const answer = queryItem();
+            const orderedAnswer = applyOrder(answer);
+            result.page = getPageMeta(orderedAnswer, page, size);
 
             const start = result.page.page * size;
             const offset = start + size;
 
-            result.data = answer.slice(start, offset);
+            result.data = orderedAnswer.slice(start, offset);
             //console.log(start, offset, result.data);
 
             resolve(result);
