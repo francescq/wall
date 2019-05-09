12284f9911914ea17c4606c6e82721c5e7164bc1
Francesc Quiones <francescq@gmail.com>
2019-05-09 17:32:16 +0200

    gzip the production bundle

--- webpack.common.js
+++ webpack.common.js
@@ -1,61 +1,62 @@
-const path = require('path');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackVisualizerPlugin = require('webpack-visualizer-plugin');
-const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const path = require('path')
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackVisualizerPlugin = require('webpack-visualizer-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
 
 module.exports = {
-    entry: {
-        app: './src/index.js',
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            title: 'Production',
-            hash: true,
-            template: './src/index.html',
-        }),
-        new WebpackVisualizerPlugin({
-            filename: '../statistics.html'
-        }),
-    ],
-    output: {
-        chunkFilename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.(s*)css$/,
-                use: ['style-loader', 'css-loader', 'sass-loader'],
-            },
-            {
-                test: [/\.eot$/, /\.ttf$/, /\.svg$/, /\.woff$/, /\.woff2$/],
-                use: [
-                    {
-                        loader: 'file-loader',
-                    },
-                ],
-            },
-            {
-                test: /\.(png|jp(e*)g|svg)$/,
-                use: [
-                    {
-                        loader: 'url-loader',
-                        options: {
-                            limit: 8000, // Convert images < 8kb to base64 strings
-                            name: 'images/[name].[ext]',
-                        },
-                    },
-                ],
-            },
-        ],
-    },
-};
+  entry: {
+    app: './src/index.js'
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Production',
+      hash: true,
+      template: './src/index.html'
+    }),
+    new WebpackVisualizerPlugin({
+      filename: '../statistics.html'
+    })
+    // new CompressionPlugin()
+  ],
+  output: {
+    chunkFilename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.(s*)css$/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      },
+      {
+        test: [/\.eot$/, /\.ttf$/, /\.svg$/, /\.woff$/, /\.woff2$/],
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: 'images/[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+}
